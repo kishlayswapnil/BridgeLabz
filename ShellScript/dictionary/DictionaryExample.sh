d4589681ab690ea5ac1202f8410b6636ddfce3ca
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-13 17:37:21 +0530

    Added: Program to see a simple example of dictionary.

---
+++ ShellScript/dictionary/DictionaryExample.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+#Demo program of dictionary.
+#Declaring dictionary.
+declare -A Imed
+
+	Imed[sunil]="bark"
+	Imed[anurag]="moo"
+	Imed[shinde]="tweet"
+	Imed[neeraj]="howl"
+	Imed[kishlay]="speak"
+	Imed[saadhu]="mew"
+
+echo ${Imed[@]}
+echo ${#Imed[@]}
+echo ${!Imed[@]}
