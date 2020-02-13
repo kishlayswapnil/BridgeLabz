6fc7c534f14365ab92d7cd27d333e395e2f63507
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-13 17:48:21 +0530

    Added: The program to check if a number is prime or not of any specific range.

---
+++ ShellScript/forloop/PrimeRange.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+#Program to check the prime number upto a desired range.
+read -p "Enter the range " end
+
+for (( value1=1; value1<=$end; value1++ ))
+do
+	prime=1
+	for (( value2=2; value2<=$value1/2; value2++ ))
+	do
+        	if (( $value1 % $value2 == 0 ))
+        	then
+               prime=$(($prime-1))
+       			break;
+        	fi
+	done
+	if (( $prime == 1 ))
+	then
+		echo "prime no" $j
+	fi
+done
